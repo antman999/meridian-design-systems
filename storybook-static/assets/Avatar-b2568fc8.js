7ba6a409c478d544f174b0f00e5d3b26e0a4b496
antman999 <anthonymendoza9922@gmail.com>
2023-07-21 16:38:03 -0400

    start of first publish

---
+++ storybook-static/assets/Avatar-b2568fc8.js
@@ -0,0 +1,28 @@
+import{j as t,a as r,F as f}from"./jsx-runtime-29545a09.js";import{M as u,b as g,C as o,d as i,A as v,D as e,e as y}from"./index-ec186e6e.js";import{A as s,S as A,a as d,I as p,L as h,C as m}from"./Avatar.stories-4a60742e.js";import{u as c}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-0add55a3.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-929668a7.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-69574f52.js";import"./_baseIsEqual-976d9d82.js";import"./assert-a1982797.js";import"./index-d37d4223.js";import"./polished.esm-2f0368bb.js";import"./extends-34e645d9.js";import"./uniq-944679ca.js";import"./index-356e4a49.js";import"./emotion-react-jsx-runtime.browser.esm-1cc7cfce.js";import"./Icon-c554d5e4.js";import"./index-8d47fad6.js";import"./styles-fc14b8d3.js";import"./animation-7bf5af4f.js";function l(a){const n=Object.assign({h1:"h1",p:"p",em:"em",h2:"h2",h3:"h3"},c(),a.components);return r(f,{children:[t(n.h1,{id:"avatar",children:"Avatar"}),`
+`,t(u,{of:s}),`
+`,t(g,{children:"Displays an image that represents a user or organization"}),`
+`,t(n.p,{children:"Use an avatar for attributing actions or content to specific users."}),`
+`,r(n.p,{children:["The user's name should ",t(n.em,{children:"always"})," be present when using Avatar – either printed beside the avatar or in a tooltip."]}),`
+`,t(o,{children:t(i,{of:A})}),`
+`,t(v,{of:s}),`
+`,t(n.h2,{id:"usage-avatar",children:"Usage Avatar"}),`
+`,t(n.p,{children:`is used to represent a person or an organization. By default the
+avatar shows an image and gracefully falls back to the first initial of the username.
+While hydrating the component you may find it useful to render a skeleton template
+to indicate that Avatar is awaiting data. Avatars can be grouped with the AvatarList
+component.`}),`
+`,t(n.h2,{id:"additional-variants",children:"Additional variants"}),`
+`,t(n.h3,{id:"sizes",children:"Sizes"}),`
+`,t(e,{of:d}),`
+`,t(i,{of:d}),`
+`,t(n.h3,{id:"initials",children:"Initials"}),`
+`,t(e,{of:p}),`
+`,t(i,{of:p}),`
+`,t(n.h3,{id:"loading",children:"Loading"}),`
+`,t(e,{of:h}),`
+`,t(i,{of:h}),`
+`,t(n.h3,{id:"playground",children:"Playground"}),`
+`,t(n.p,{children:"Interact with the component and see how it responds to the different input properties."}),`
+`,t(o,{children:t(i,{of:m})}),`
+`,t(y,{of:m})]})}function H(a={}){const{wrapper:n}=Object.assign({},c(),a.components);return n?t(n,Object.assign({},a,{children:t(l,a)})):l(a)}export{H as default};
+//# sourceMappingURL=Avatar-b2568fc8.js.map
---
+++ storybook-static/assets/emotion-use-insertion-effect-with-fallbacks.browser.esm-929668a7.js
@@ -0,0 +1,2 @@
+import{r as s,a}from"./index-76fb7be0.js";var r=function(t){return t()},e=a["useInsertionEffect"]?a["useInsertionEffect"]:!1,f=e||r,o=e||s.useLayoutEffect;export{f as a,o as u};
+//# sourceMappingURL=emotion-use-insertion-effect-with-fallbacks.browser.esm-929668a7.js.map
